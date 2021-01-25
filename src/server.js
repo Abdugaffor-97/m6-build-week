0cda21afa61719404f37125a556b27482fcaa05e
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2021-01-25 11:26:40 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const {
+  badRequestHandler,
+  notFoundHandler,
+  genericErrorHandler,
+} = require("./errorHandlers");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(badRequestHandler);
+app.use(notFoundHandler);
+app.use(genericErrorHandler);
 
 const port = process.env.PORT;
 mongoose
