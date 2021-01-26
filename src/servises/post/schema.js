741df3a586994c697e482e3b29b1bb579f051530
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-01-26 17:36:17 +0500

    Addded comments

--- src/servises/post/schema.js
+++ src/servises/post/schema.js
@@ -2,13 +2,19 @@ const { Schema, model } = require("mongoose");
 
 const PostSchema = new Schema(
   {
-    text: { type: String},
-    userName: { type: String},
-    user:{type:Schema.Types.ObjectId, ref:'Profile'},
+    text: { type: String },
+    userName: { type: String },
+    user: { type: Schema.Types.ObjectId, ref: "Profile" },
+    comments: [
+      {
+        userId: { type: Schema.Types.ObjectId, ref: "Profile" },
+        text: { type: String, required: true },
+      },
+    ],
   },
   { timestamps: true }
 );
 
-const PostModel = model("Post", PostSchema)
+const PostModel = model("Post", PostSchema);
 
 module.exports = PostModel;
